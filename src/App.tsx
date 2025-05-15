3e6d29cf54f7fa407d807a3d2c5af7dd48c8bcab
glrmrissi <guilhermerissi04112006@gmail.com>
2025-05-15 02:47:15 -0300

    deploy

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import './components/Keyboard/KeyBoard.css'
+import KeyBoard from './components/Keyboard/KeyBoard'
+
+function App() {
+
+
+  return (
+    <>
+      <KeyBoard></KeyBoard>
+    </>
+  )
+}
+
+export default App
